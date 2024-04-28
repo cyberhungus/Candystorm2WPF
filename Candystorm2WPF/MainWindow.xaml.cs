7f2a942a9a60fbe6220c17647abaa5ae5e121ccc
Karl Hungus <vhkchvuoudgcwwqkxk@miucce.online>
2024-04-28 12:53:21 +0200

    Projektdateien hinzufügen.

---
+++ Candystorm2WPF/MainWindow.xaml.cs
@@ -0,0 +1,160 @@
+﻿using RobotUICSharp;
+using System;
+using System.Collections.Generic;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Candystorm2WPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        VideoPlayerWindow videoPlayerWindow;
+        SerialConnector SerialConnect;
+        public MainWindow()
+        {
+            InitializeComponent();
+
+        }
+
+        private void Candystorm_V2_Loaded(object sender, RoutedEventArgs e)
+        {
+            videoPlayerWindow = new VideoPlayerWindow("AnimationA.mp4", "AnimationB.mp4");
+            videoPlayerWindow.Show();
+            videoPlayerWindow.PlaybackFinishedEvent += PlaybackFinishedHandler;
+
+
+            SerialConnect = new SerialConnector();
+            autoConnectSerial();
+        }
+
+        private void PlaybackFinishedHandler(object? sender, EventArgs e)
+        {
+            Console.WriteLine("Playbackfinished event processed");
+            turnHardwareOff();
+        }
+
+        private void VideoTest_Click(object sender, RoutedEventArgs e)
+        {
+            turnHardwareOn();
+            videoPlayerWindow.playVideoSingle();
+        }
+
+        private void HardwareOn_Click(object sender, RoutedEventArgs e)
+        {
+            turnHardwareOn();
+        }
+
+        private void HardwareOff_Click(object sender, RoutedEventArgs e)
+        {
+        turnHardwareOff();
+        }
+
+        void turnHardwareOn()
+        {
+            if (SerialConnect.isOpen())
+            {
+                SerialConnect.sendMessage("<SYS-001>");
+            }
+        }
+
+        void turnHardwareOff()
+        {
+            if (SerialConnect.isOpen())
+            {
+                SerialConnect.sendMessage("<SYS-000>");
+            }
+        }
+
+
+        private void autoConnectSerial()
+        {
+            String[] AvailablePorts = SerialPort.GetPortNames();
+            try
+            {
+                SerialConnect.setPort(AvailablePorts[0]);
+
+                SerialConnect.openPort();
+                SerialConnect.MessageReceivedEvent += ReceiveMessage;
+
+                ArduinoLabel.Background = Brushes.Green;
+                ArduinoLabel.Text = "Connected on Port: " + SerialConnect.getPort();
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                ArduinoLabel.Background = Brushes.Red;
+            }
+            if (AvailablePorts.Length == 0)
+            {
+                ArduinoLabel.Background = Brushes.Red;
+                ArduinoLabel.Text = "No COM Devices";
+            }
+        }
+
+        private void ReceiveMessage(object sender, MessageReceivedEventArgs e)
+        {
+            Console.WriteLine(e.Message);
+            String rec = ""; 
+            try
+            {
+                rec = e.Message.Substring(0, 6);
+
+         
+                if (compareStrings(rec, "<0001>"))
+                {
+                    videoPlayerWindow.Dispatcher.Invoke(new Action(() => { videoPlayerWindow.playVideoSingle(); }));
+                   
+                    turnHardwareOn();
+                    
+
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+        }
+        bool compareStrings(String A, String B)
+        {
+            bool toReturn = false;
+            if (A.Length == B.Length)
+            {
+                for (int i = 0; i < A.Length; i++)
+                {
+                    if (A[i] == B[i])
+                    {
+                        toReturn = true;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+                return toReturn;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+
+    }
+}
---
+++ Candystorm2WPF/VideoPlayerWindow.xaml.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using static RobotUICSharp.SerialConnector;
+
+namespace Candystorm2WPF
+{
+    /// <summary>
+    /// Interaktionslogik für VideoPlayerWindow.xaml
+    /// </summary>
+    public partial class VideoPlayerWindow : Window
+    {
+        bool isSingleVideoPlaying = false;
+
+        string loopFileName = "";
+        string singleFileName = "";
+
+        public event EventHandler PlaybackFinishedEvent;
+
+        public VideoPlayerWindow(string loopVideoPath, string singleVideoPath)
+        {
+            InitializeComponent();
+            loopFileName = loopVideoPath;
+            singleFileName = singleVideoPath;
+            playVideoLoop();
+
+
+
+        }
+
+        public void playVideoLoop()
+        {
+            isSingleVideoPlaying= false;
+            VideoPlayer.Source = new Uri(System.IO.Directory.GetCurrentDirectory() + "/" + loopFileName);
+            VideoPlayer.Play();
+            this.WindowStyle = WindowStyle.None;
+            this.WindowState = WindowState.Maximized;
+        }
+
+        public void playVideoSingle()
+        {
+            isSingleVideoPlaying=true;
+            VideoPlayer.Source = new Uri(System.IO.Directory.GetCurrentDirectory() + "/" + singleFileName);
+            VideoPlayer.Play();
+            this.WindowStyle = WindowStyle.None;
+            this.WindowState = WindowState.Maximized;
+        }
+
+        private void VideoPlayer_MediaEnded(object sender, RoutedEventArgs e)
+        {
+            if (isSingleVideoPlaying)
+            {
+                playVideoLoop();
+                PlaybackFinishedEvent.Invoke(this, new EventArgs());
+            }
+            else
+            {
+                VideoPlayer.Position = new TimeSpan(0, 0, 1);
+                VideoPlayer.Play();
+            }
+        }
+
+        private void VideoPlayer_MediaFailed(object sender, ExceptionRoutedEventArgs e)
+        {
+            Console.WriteLine(e.ToString());
+        }
+
+        private void VideoPlayer_MediaOpened(object sender, RoutedEventArgs e)
+        {
+            Console.WriteLine(e.Source.ToString());
+        }
+    }
+}
